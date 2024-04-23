f392bf4fcfbb6e87f12557750629a10bce9a09e6
Californ1a <rk_california@yahoo.com>
2024-04-23 08:53:13 -0400

    update release

--- release.ps1
+++ release.ps1
@@ -8,18 +8,18 @@ else {
 
 foreach($target in ("BepInEx5"))
 {
-    $copy = $dir + "copy\BepInEx\plugins\ProfileFixer\"
+    $copy = $dir + "copy\BepInEx\plugins\SaveFolderEditor\"
 
     Remove-Item -Force -Path ($copy) -Recurse -ErrorAction SilentlyContinue
     New-Item -ItemType Directory -Force -Path ($copy)
 
-	Copy-Item -Path ($PSScriptRoot + "\ProfileFixer\bin\Debug\ProfileFixer.dll") -Destination ($copy) -Recurse -Force
+	Copy-Item -Path ($PSScriptRoot + "\SaveFolderEditor\bin\Debug\SaveFolderEditor.dll") -Destination ($copy) -Recurse -Force
 
     Copy-Item -Path ($dir + "\..\README.md") -Destination ($copy) -Recurse -Force
     Copy-Item -Path ($dir + "\..\LICENSE") -Destination ($copy) -Recurse -Force
 
-    $ver = (Get-ChildItem -Path ($PSScriptRoot + "\ProfileFixer\bin\") -Filter ("*ProfileFixer.dll") -Recurse -Force)[0].VersionInfo.FileVersion.ToString() -replace "([\d+\.]+?\d+)[\.0]*$", '${1}'
-    Compress-Archive -Path ($dir + "copy\BepInEx") -Force -CompressionLevel "Optimal" -DestinationPath ($dir + "Distance.ProfileFixer" +"_v" + $ver + ".zip")
+    $ver = (Get-ChildItem -Path ($PSScriptRoot + "\SaveFolderEditor\bin\") -Filter ("*SaveFolderEditor.dll") -Recurse -Force)[0].VersionInfo.FileVersion.ToString() -replace "([\d+\.]+?\d+)[\.0]*$", '${1}'
+    Compress-Archive -Path ($dir + "copy\BepInEx") -Force -CompressionLevel "Optimal" -DestinationPath ($dir + "Distance.SaveFolderEditor" +"_v" + $ver + ".zip")
 }
 
 Remove-Item -Force -Path ($dir + "copy") -Recurse -ErrorAction SilentlyContinue
